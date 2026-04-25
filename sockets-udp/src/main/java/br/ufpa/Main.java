0643c596a9ed95771e30300143eca0c61d2e0393
DaniloSovano <danilosovano@gmail.com>
2026-04-24 23:07:51 -0300

    Primeiro commit: Cliente UDP

--- sockets-udp/src/main/java/br/ufpa/Main.java
+++ sockets-udp/src/main/java/br/ufpa/Main.java
@@ -1,13 +1,25 @@
 package br.ufpa;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
-/**
- * Hello world!
- *
- */
-public class Main 
-{
+public class Main {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {   
+        try{
+        String address = "127.0.0.1";
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress serverAddress = InetAddress.getByName(address);
+        int port = 12000;
+        String message = "Ping";
+        byte[] sendData = message.getBytes();
+        
+        DatagramPacket packet = new DatagramPacket(sendData, sendData.length,serverAddress,port);
+
+        socket.send(packet);
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
